6771e347115c17427580ddec55fd095844a64378
Kelvin Muthama <kelvinkamau387@gmail.com>
2023-10-26 16:36:02 +0300

    Fix: Change github to google

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -108,7 +108,7 @@ const AuthForm = () => {
             />
             <AuthSocialButton
               icon={BsGoogle}
-              onClick={() => socialAction("github")}
+              onClick={() => socialAction("google")}
             />
           </div>
         </div>
