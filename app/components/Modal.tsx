05405d384979972bf64f67bcd89e6da4cdbad7b6
Kelvin Muthama <kelvinkamau387@gmail.com>
2023-11-09 15:21:41 +0300

    Fix: Add onClose to button

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -41,6 +41,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
                   <button
                     type="button"
                     className="rounded bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                    onClick={onClose}
                   >
                     <span className="sr-only">Close</span>
                     <IoClose className="h-6 w-6" />
